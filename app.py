857f489c4d1b786e0e86b3fe0ed1948a9cb82897
Lucas Pamplona Soares <69314618+LucasPamplonaSoares@users.noreply.github.com>
2022-03-01 18:11:03 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import requests
+from tkinter import *
+
+
+def pegar_cotacoes():  # Função que vai dar ação para algo, tipo ao clicar o botão vai fazer a ação de buscar a cotação
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+
+    texto_cotacao["Text"] = texto
+
+
+
+janela = Tk()  # Inicio da Janela
+janela.title('Teste')  # Titulo da Janela
+texto_orientacao = Label(janela, text='Janela Teste')  # Por o Texto
+texto_orientacao.grid(column=0, row=0)  # Por numa posição
+
+botao = Button(janela, text='Clica aqui', command=pegar_cotacoes)
+botao.grid(column=0, row=1)
+
+texto_cotacao = Label(janela, text='')
+texto_cotacao.grid(column=0, row=2)
+janela.mainloop()  # Fim da janela
---
+++ estoque_venda.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import ttk
+# import datetime as dt
+import psycopg2
+
+tipo_lista = ['Caixa', 'Unidade', 'Kg']
+lista_produto = []
+janela = tk.Tk()
+
+try:
+    conn = psycopg2.connect(database="postgres", user="postgres", password="1234", host="localhost")
+    print("connected")
+except:
+    print("I am unable to connect to the database")
+cur = conn.cursor()
+
+
+def criarTabela():
+        cur.execute("CREATE TABLE estoque (id serial PRIMARY KEY, descricao char, tipo_unidade char, quantidade integer);")
+
+
+def inserir_produto():  # Criar a função
+    descricao = entry_descricao.get()  # Pega as informações que eu inseri
+    tipo = combobox_selecionar.get()
+    quantidade = entry_quantidade.get()
+    # data_criacao = dt.datetime.now()  # Ao cadastrar vai salvar a hora e o dia que foi criado
+    # data_criacao = data_criacao.strftime('%d/%m/%y %H:%M')  # COnverti para mostrar num formato em dia mes e ano e hora
+    # codigo = len(lista_produto) + 1  # Cria a lista de codigo
+    # codigo_srt = "Cod-{}".format(codigo)  # Formata
+    # lista_produto.append((codigo_srt, descricao, tipo, quantidade, data_criacao))  # Criação de uma tupla imutavel
+    cur.execute("INSERT INTO estoque(descricao, tipo_unidade, quantidade) VALUES (%s, %s, %s);", (descricao, tipo, quantidade))
+    conn.commit()
+
+
+
+janela.title('Estoque Mercearia Luz')  # Titulo da janela
+
+label_descricao = tk.Label(text='Descrição do Produto')
+label_descricao.grid(column=0, row=1, padx=10, pady=10, sticky='nswe', columnspan=4)
+
+entry_descricao = tk.Entry()  # Entrada de dados
+entry_descricao.grid(column=0, row=2, padx=10, pady=10, sticky='nswe', columnspan=4)
+
+label_tipo_unidade = tk.Label(text='Tipo da Unidade')
+label_tipo_unidade.grid(column=0, row=3, padx=10, pady=10, sticky='nswe', columnspan=2)
+
+combobox_selecionar = ttk.Combobox(values=tipo_lista)
+combobox_selecionar.grid(column=2, row=3, padx=10, pady=10, sticky='nswe', columnspan=2)
+
+label_quantidade = tk.Label(text='Quantidade do Produto')
+label_quantidade.grid(column=0, row=4, padx=10, pady=10, sticky='nswe', columnspan=2)
+
+entry_quantidade = tk.Entry()
+entry_quantidade.grid(column=2, row=4, padx=10, pady=10, sticky='nswe', columnspan=2)
+
+botao_cadastrar_produto = tk.Button(text='Cadastrar', command=inserir_produto)
+botao_cadastrar_produto.grid(column=0, row=5, padx=10, pady=10, sticky='nswe', columnspan=4)
+
+
+janela.mainloop()
